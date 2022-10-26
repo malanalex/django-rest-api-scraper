1566d6abe940ee3de65d6eb6b2937a93e506d409
alexmalan <alexmalan16@gmail.com>
2022-10-26 11:47:31 +0300

    id10: update unit tests for endpoints

--- apps/pages/tests/tests.py
+++ apps/pages/tests/tests.py
@@ -2,6 +2,7 @@
 Unit tests for the pages app.
 """
 from pathlib import Path
+from django.urls import reverse
 
 import lxml.html
 from rest_framework.test import APITestCase
@@ -10,14 +11,19 @@ from apps.pages.models import Page, PageLink
 from apps.pages.tasks import crawl_data, parse_data
 
 
-class PageServiceTaskTests(APITestCase):
+class PageTests(APITestCase):
     """
-    Test task services.
+    Test page services and endpoints.
     """
 
     page_1 = {
         "id": 1,
         "url": "https://www.lipsum.com/",
+    }
+
+    page_2 = {
+        "id": 2,
+        "url": "https://www.yahoo.com/",
     }
 
     common_tags = ["html", "body", "p", "div"]
@@ -69,3 +75,73 @@ class PageServiceTaskTests(APITestCase):
 
         page_updated = Page.objects.filter(id=page.id).first()
         self.assertEqual(page_updated.stats['total_links'], page_links)
+
+    def test_delete_success(self):
+        """
+        Test delete page.
+        """
+        # save page to db
+        page = Page(url=self.page_1["url"], stats={"total_links": 0, "unique_links": 0})
+        page.save()
+
+        # check page exists
+        page_exists = Page.objects.filter(id=page.id).exists()
+        self.assertTrue(page_exists)
+
+        # delete page
+        page_delete = reverse("page-update-delete", kwargs={"page_id": page.id})
+        response = self.client.delete(page_delete, format="json")
+
+        self.assertTrue(response.status_code, 204)
+    
+    def test_update_success(self):
+        """
+        Test update page.
+        """
+        # save page to db
+        page = Page(url=self.page_1["url"], stats={"total_links": 0, "unique_links": 0})
+        page.save()
+
+        # check page exists
+        page_exists = Page.objects.filter(id=page.id).exists()
+        self.assertTrue(page_exists)
+
+        payload = {
+            "url": "https://www.theverge.com/",
+        }
+
+        # update page
+        page_delete = reverse("page-update-delete", kwargs={"page_id": page.id})
+        response = self.client.put(page_delete, payload, format="json")
+
+        self.assertTrue(response.status_code, 200)
+        self.assertEqual(response.data["url"], payload["url"])
+
+    def test_list_pages_success(self):
+        """
+        Test list pages.
+        """
+        # save page to db
+        page_1 = Page(url=self.page_1["url"], stats={"total_links": 0, "unique_links": 0})
+        page_1.save()
+
+        page_2 = Page(url=self.page_2["url"], stats={"total_links": 0, "unique_links": 0})
+        page_2.save()
+
+        # check page exists
+        page_exists_1 = Page.objects.filter(id=page_1.id).exists()
+        self.assertTrue(page_exists_1)
+        page_exists_2 = Page.objects.filter(id=page_2.id).exists()
+        self.assertTrue(page_exists_2)
+
+        # list pages
+        page_list = reverse("pages-list")
+        response = self.client.get(page_list, format="json")
+        res_json = response.json()
+
+        self.assertTrue(response.status_code, 200)
+        self.assertEqual(len(res_json), 2)
+        self.assertEqual(res_json[0]['url'], self.page_1['url'])
+        self.assertEqual(res_json[1]['url'], self.page_2['url'])
+
+
